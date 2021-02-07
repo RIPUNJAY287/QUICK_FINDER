1518214aa00df68f1e2ac50728fa48bc5ef5c301
RIPUNJAY287 <kripunjay81@gmail.com>
2021-02-08 00:54:30 +0530

    Updates

--- backend/models/upload_model.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const schema = mongoose.Schema;
-
-const uploadSchema = new schema({
-  image_name:String,
-});
-
-const uploadModel = mongoose.model('Upload_Images',uploadSchema);
-module.exports = uploadModel;
---
+++ precache-manifest.5537f116fc37d79a6da3cae125772fec.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b1af4319597df692b79682a1cefcce7",
+    "url": "/QUICK_FINDER/index.html"
+  },
+  {
+    "revision": "a4ebcee19280269943e7",
+    "url": "/QUICK_FINDER/static/css/2.8d3f7698.chunk.css"
+  },
+  {
+    "revision": "713ff1abcafff60ed3a6",
+    "url": "/QUICK_FINDER/static/css/main.b55c25b9.chunk.css"
+  },
+  {
+    "revision": "a4ebcee19280269943e7",
+    "url": "/QUICK_FINDER/static/js/2.8d52df6d.chunk.js"
+  },
+  {
+    "revision": "5660b2ecb2c25095ecd8e990d367edf2",
+    "url": "/QUICK_FINDER/static/js/2.8d52df6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "713ff1abcafff60ed3a6",
+    "url": "/QUICK_FINDER/static/js/main.d87a1b55.chunk.js"
+  },
+  {
+    "revision": "9dfd09a7597132b60813",
+    "url": "/QUICK_FINDER/static/js/runtime-main.0c320524.js"
+  },
+  {
+    "revision": "5d4734ef5e7cf2b926436da761efd9f3",
+    "url": "/QUICK_FINDER/static/media/loginpic.5d4734ef.jpg"
+  }
+]);
